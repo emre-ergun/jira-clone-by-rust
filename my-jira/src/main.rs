f01b4acf2f14a371a600c241a140611eb0f89413
Emre Ergun <emreergun@engramsoft.com>
2023-12-19 13:31:35 +0300

    step3: add crud operations for epic/story completed

--- my-jira/src/main.rs
+++ my-jira/src/main.rs
@@ -1,14 +1,6 @@
 mod models;
 mod db;
 
-use db::JSONFileDatabase;
-use db::Database;
-
 fn main() {
-    let json_file = JSONFileDatabase {
-        file_path: "data/db.json".to_owned()
-    };
-
-    let deserialized = json_file.read_db().unwrap();
-    println!("{deserialized:#?}");
+    println!("Welcome To My-Jira!");
 }
